f2e92398a4888bcd016ef76cbc82161268e3f38d
Santi <santiago.gonzalezagudelo@gmail.com>
2023-03-19 12:07:44 -0500

    Finished :D

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const fecha=document.querySelector('.fecha')
 const email=document.querySelector('#email')
 const contra=document.querySelector('#contra')
 const contraOtra=document.querySelector('#contraOtra')
+let bien=0
 
 const registrar=document.getElementsByClassName('registrar')
 const nombreValida=/^[a-zA-Z]+\D$/g
@@ -14,17 +15,19 @@ const telValida=/[3]\d{9}$/g
 const emailValida=/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
 
 function nombreVal(){
+    bien=0
     if(nombreValida.test(nombres.value)){
         console.log(nombres.value)
+        bien+=1
     }
     else{
         alert('Error el nombre no debe contener símbolos ni números')
         nombres.value=''
-
     }
 
     if(apellidoValida.test(apellido.value)){
         console.log(apellido.value)
+        bien+=1
     }else{
         alert('Error el apellido no debe contener símbolos ni números')
         apellido.value=''
@@ -32,13 +35,15 @@ function nombreVal(){
 
     if(fechaValida.test(fecha.value)){
         console.log(fecha.value)
+        bien+=1
     }else{
-        alert('Mal')
+        alert('Error en la fecha, digítela de nuevo')
         fecha.value=''
     }
 
     if(telValida.test(tel.value)){
         console.log(tel.value)
+        bien+=1
     }else{
         alert('Error, el número debe ser de 10 dígitos y solo contener números')
         tel.value=''
@@ -46,7 +51,7 @@ function nombreVal(){
 
     if(emailValida.test(email.value)){
         console.log(email.value)
-
+        bien+=1
     }else{
         alert('Error en el correo electrónico')
         email.value=''
@@ -54,9 +59,18 @@ function nombreVal(){
 
     if(contra.value==contraOtra.value){
         console.log(contraOtra.value)
-
+        bien+=1
     }else{
         alert('Error, las contraseñas no coinciden')
         contraOtra.value=''
     }
+
+    if(bien==6){
+        alert('Usuario Registrado')
+        location.href='./index.html'
+    }
+}
+
+function cancelar(){
+    location.href='./index.html'
 }
